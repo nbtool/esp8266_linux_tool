117d76867cc96d429e8d95b7c33d199e546f5275
beautifulzzzz <beautifulzzzz@qq.com>
2020-09-09 09:18:29 +0800

    update pwm demo

--- app/app_pwm/main/pwm_example_main.c
+++ app/app_pwm/main/pwm_example_main.c
@@ -26,6 +26,8 @@
 
 
 #define PWM_1_OUT_IO_NUM    14
+#define PWM_2_OUT_IO_NUM    12
+#define PWM_3_OUT_IO_NUM    2
 
 // PWM period 10us(100Khz), same as depth
 #define PWM_PERIOD    (20000)
@@ -33,31 +35,42 @@
 static const char *TAG = "pwm_example";
 
 // pwm pin number
-const uint32_t pin_num[1] = {
-    PWM_1_OUT_IO_NUM
+const uint32_t pin_num[3] = {
+    PWM_1_OUT_IO_NUM,
+    PWM_2_OUT_IO_NUM,
+    PWM_3_OUT_IO_NUM,
 };
 
 // dutys table, (duty/PERIOD)*depth
-uint32_t duties[1] = {
-    18700
+uint32_t duties[3] = {
+    1400,
+    1300,
+    1600
 };
 
 // phase table, (phase/180)*depth
-int16_t phase[1] = {
+int16_t phase[3] = {
+    0,
+    0,
     0
 };
 
 void app_main()
 {
-    pwm_init(PWM_PERIOD, duties, 1, pin_num);
-    pwm_set_channel_invert(0x1 << 0);
+    pwm_init(PWM_PERIOD, duties, 3, pin_num);
+//    pwm_set_channel_invert(0x1 << 0);
+//    pwm_set_channel_invert(0x1 << 1);
     pwm_set_phases(phase);
     pwm_start();
 
     int t = 0;
     while (1) {
         if(t%3 == 0){
-            duties[0] = duties[0] == 18000 ? 18700:18000;
+            duties[1] = duties[1] == 2000 ? 1300:2000;
+            pwm_set_duty(1,duties[1]);
+            pwm_start();
+        }else if(t%5 == 0){
+            duties[0] = duties[0] == 1900 ? 1400:1900;           
             pwm_set_duty(0,duties[0]);
             pwm_start();
         }
